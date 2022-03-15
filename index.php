8bbbd993740abe1321fba40522517752b79ae66c
romartiny <91370772+romartiny@users.noreply.github.com>
2022-03-15 22:27:17 +0300

    check database

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysqli_connect('localhost', 'root', 'root', 'social');
+
+if(mysqli_connect_errno()) {
+    echo  'error with: ' . mysqli_connect_errno();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Go on</p>
+    
+</body>
+</html>
